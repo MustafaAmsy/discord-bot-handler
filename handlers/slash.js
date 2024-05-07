31bea6dcdff649c317601b96db7117363cd152b7
MustafaAmsy <64412145+MustafaAmsy@users.noreply.github.com>
2024-05-07 18:24:05 +0300

    update handlers/slash.js

--- handlers/slash.js
+++ handlers/slash.js
@@ -4,5 +4,7 @@ const { AsciiTable3 } = require('ascii-table3');
 const table = new AsciiTable3().setHeading('Slash Commands', 'Load Status')
 module.exports =  async(client) => { 
   const commands = [];
-  fs.readdirSync('./Commands/Slash/').forEach(folder => { })
+  fs.readdirSync('./Commands/Slash/').forEach(folder => {
+   const commandFiles = [];
+    })
 }
