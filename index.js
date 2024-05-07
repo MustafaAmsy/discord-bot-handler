95d71e6ebf5d7f0bdfb7a308b6f9f3b3b0512ce7
MustafaAmsy <64412145+MustafaAmsy@users.noreply.github.com>
2024-05-07 18:10:54 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const config = require('./configuration.json')
 const client = new Client({
   intents:
   [
   GatewayIntentBits.Guilds  
     ]
 });
+client.login(config.token)
