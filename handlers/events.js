8530489eb41274ef9ce465f49475e7475b49a353
MustafaAmsy <64412145+MustafaAmsy@users.noreply.github.com>
2025-01-23 17:34:27 +0300

    update handlers/events.js

--- handlers/events.js
+++ handlers/events.js
@@ -1,2 +1,23 @@
 const { AsciiTable3 } = require('ascii-table3');
 const table = new AsciiTable3().setHeading('Events', 'Load Status');
+module.exports = async(client) => {
+  function loadEvents(directory) {
+    const files = fs.readdirSync(directory, { withFileTypes: true });
+    for (const file of files) {
+      const fullPath = path.join(directory,file.name);
+      if(file.isDirectory()) {
+        loadEvents(fullPath)
+      } else if(file.isFile() && file.name.endsWith('js')) {
+        const command = require(fullPath);
+        if('data' in command && 'execute' in command) {
+        client.slashCommands.set(command.data.name,command);
+        commands.push(command.data.toJSON());
+        table.addRow(command.data.name, '✔')
+        } else {
+          table.addRow(file.name, '✖')
+        }
+      }
+    }
+  }
+  const commandsPath = path.join(__dirname, 'commands/slash/');
+  loadCommands(commandsPath);
