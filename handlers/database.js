dce5b5d94721a5897ff662adb627222b429be43d
MustafaAmsy <64412145+MustafaAmsy@users.noreply.github.com>
2025-01-25 00:21:50 +0300

    update handlers/database.js

--- handlers/database.js
+++ handlers/database.js
@@ -1,4 +1,7 @@
 const { MongoClient } = require('mongodb');
-module.exports = (client) => {
-  
+const colors = require('colors');
+module.exports = async(client) => {
+  const databaseClient = new MongoClient(client.config.database.uri);
+  databaseClient.connect().then(() => console.log(colors.cyan('Succesfully connected to the database')));
+  const database = databaseClient.db('main');
 }
