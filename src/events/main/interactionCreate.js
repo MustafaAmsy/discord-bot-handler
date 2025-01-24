5f8ce62360ab2abc0dd6e4c1285ed37cedd7055b
MustafaAmsy <64412145+MustafaAmsy@users.noreply.github.com>
2025-01-24 21:08:41 +0300

    update src/events/main/interactionCreate.js

--- src/events/main/interactionCreate.js
+++ src/events/main/interactionCreate.js
@@ -13,7 +13,6 @@ module.exports = {
    if(interaction.isChatInputCommand()) {
      const command = client.slashCommands.get(interaction.commandName);
      if(!command) return;
-     const subcommand = interaction.options.getSubcommand();
      if(command.dev && !isDeveloper) return await interaction.reply({ content: 'This is a developer command only', flags: MessageFlags.Ephemeral});
      try { 
      await command.execute({ interaction, client });
@@ -73,4 +72,22 @@ module.exports = {
      }
    }
   }
+}
+async function loadSubcommand(interaction, client) {
+  const subcommand = client.subcommands.get(`${interaction.commandName}.${interaction.customId}`);
+  if(!subcommand) return;
+   const developers = client.config.developers;
+   let isDeveloper = false;
+   if(!Array.isArray(developers) && interaction.author.id === developers) {
+     isDeveloper = true;
+   } else if(Array.isArray(developers) && developers.includes(interaction.author.id)) {
+     isDeveloper = true;
+   }
+   if(subcommand.dev && !isDeveloper) return await interaction.reply({ content: 'This is a developer command only', flags: MessageFlags.Ephemeral});
+   try { 
+     await subcommand.execute({ interaction, client });
+   } catch(error) {
+     console.log(error);
+     await interaction.reply({ content: 'Something has went wrong', flags: MessageFlags.Ephemeral });
+   }
 }
