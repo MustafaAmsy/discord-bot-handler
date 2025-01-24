3fcf4eadb76c966a0ddfcdd509bc8d19658ed619
MustafaAmsy <64412145+MustafaAmsy@users.noreply.github.com>
2025-01-24 17:37:48 +0300

    update src/events/main/interactionCreate.js

--- src/events/main/interactionCreate.js
+++ src/events/main/interactionCreate.js
@@ -3,9 +3,18 @@ module.exports = {
   name: 'interactionCreate',
   on: true,
   execute: async({ interaction, client }) => {
-    switch(interaction.type) {
-      case InteractionType.ApplicationCommand:
-        
-    }
+   const developers = client.config.developers;
+   let isDeveloper = false;
+   if(!Array.isArray(developers) && interaction.author.id === developers) {
+     isDeveloper = true;
+   } else if(Array.isArray(developers) && developers.includes(interaction.author.id)) {
+     isDeveloper = true;
+   }
+   if(interaction.isChatInputCommand()) {
+     const command = client.slashCommands.get(interaction.commandName);
+     if(!command) return;
+     if(command.dev && !isDeveloper) return;
+     await command.execute({ interaction, client })
+   }
   }
 }
