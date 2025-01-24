731cf9e05ad15895edbef947ac433b07418a5ec2
MustafaAmsy <64412145+MustafaAmsy@users.noreply.github.com>
2025-01-25 00:26:12 +0300

    update handlers/database.js

--- handlers/database.js
+++ handlers/database.js
@@ -4,4 +4,12 @@ module.exports = async(client) => {
   const databaseClient = new MongoClient(client.config.database.uri);
   databaseClient.connect().then(() => console.log(colors.cyan('Succesfully connected to the database')));
   const database = databaseClient.db('main');
+  const global = database.collection('global');
+  const guild = database.collection('guild');
+  const channel = database.collection('channel');
+  const user = database.collection('user');
+  const member = database.collection('member');
+  const message = database.collection('message');
+  const command = database.collection('command');
+  client.db = { global, guild, channel, user, member, message, command, database };
 }
