2635ab43796bc06d4160b3018bdc0c9aa85445bd
MustafaAmsy <64412145+MustafaAmsy@users.noreply.github.com>
2025-01-24 09:18:15 +0300

    update src/events/main/messageCreate.js

--- src/events/main/messageCreate.js
+++ src/events/main/messageCreate.js
@@ -3,10 +3,28 @@ module.exports = {
   name: 'messageCreate',
   on: true,
   execute: async ({ message, client }) => {
+  const developers = client.config.developers;
+  let isDeveloper = false;
+  if(Array.isArray(developers)) {
+    if(developers.includes(message.author.id.toString())) {
+      isDeveloper = true;
+    }
+  } else if(developers === message.author.id) {
+    isDeveloper = true;
+  }
   const prefix = client.config.prefix;
   if(message.author.bot) return;
   if(!message.content.startsWith(prefix)) return;
-  
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  if(command.length === 0) return;
+  const cmd = client.commands.get(command);
+  if(cmd) {
+    try {
+     if(cmd.dev) {
+       
+    }
+  }
   
   }
 }
