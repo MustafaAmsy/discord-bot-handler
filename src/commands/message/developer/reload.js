712a723167be49d0caee6e07633672ccdad37707
MustafaAmsy <64412145+MustafaAmsy@users.noreply.github.com>
2025-01-26 20:42:41 +0300

    update src/commands/message/developer/reload.js

--- src/commands/message/developer/reload.js
+++ src/commands/message/developer/reload.js
@@ -10,7 +10,7 @@ client.aliases.clear();
 client.components.buttons.clear();
 client.components.modals.clear();
 client.components.selectmenus.clear();
-client.comppnents.contextmenus.clear();
+client.components.contextmenus.clear();
 client.components.autocomplete.clear();
 client.removeAllListeners();
 ['slash', 'commands', 'components', 'events'].forEach(handler => require(`../../../../handlers/${handler}`)(client));
